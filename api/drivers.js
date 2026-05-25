84865a407c0e7eeacd8a5ed655dc4f9ed053ae00
paullogeart20 <paul.logeart@fleetch.net>
2026-05-25 18:44:29 +0000

    Add Vercel API - drivers

---
+++ api/drivers.js
@@ -0,0 +1,21 @@
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwZePDKjBd1ALG9vhGPgvtfZe2uio7S5uqbyKfSYdJHLMkIqUuBGUQSeP4cMYB-Xfjn/exec';
+
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  try {
+    const response = await fetch(APPS_SCRIPT_URL, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    });
+    const data = await response.json();
+    return res.status(200).json(data);
+  } catch (err) {
+    return res.status(500).json({ status: 'ERROR', message: err.message });
+  }
+}
