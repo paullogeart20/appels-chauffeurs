4ff578a46e8749a4a6d8f79d3555b54895ea1884
paullogeart20 <paul.logeart@fleetch.net>
2026-05-26 08:48:40 +0000

    Support responses sheet param

--- api/drivers.js
+++ api/drivers.js
@@ -3,16 +3,16 @@ const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwZePDKjBd1ALG9
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
+
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
+  const sheet = req.query.sheet || 'drivers';
+  const url = sheet === 'responses'
+    ? APPS_SCRIPT_URL + '?sheet=responses'
+    : APPS_SCRIPT_URL;
 
   try {
-    const response = await fetch(APPS_SCRIPT_URL, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' }
-    });
+    const response = await fetch(url, { method: 'GET' });
     const data = await response.json();
     return res.status(200).json(data);
   } catch (err) {
